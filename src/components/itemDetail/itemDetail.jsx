841bc9fadc7d3976ed84978fa4a250611c94adb6
MicaelaLagana <lagana.mcl@gmail.com>
2022-03-13 21:48:08 -0300

    fixed style

--- src/components/itemDetail/itemDetail.jsx
+++ src/components/itemDetail/itemDetail.jsx
@@ -1,25 +1,30 @@
 import {React} from 'react'
-import { Row, Card } from 'react-bootstrap'
+import { Row, Card, Container } from 'react-bootstrap'
 import ItemCount from '../itemCount/itemCount'
 
 
 export default function ItemDetail(props) {
+    const size ="300px";
     console.log("props", props);
   return (
-    <Row>
-        <img src={props.image} alt=""/>
-        <Card>
-            <Card.Title as="h5">{props.title}</Card.Title>
-            <Card.Body>
-                <Card.Text>
-                    {props.description}
-                </Card.Text>
-                <Card.Text>
-                    {props.price}
-                </Card.Text>
-                <Row><ItemCount initial={1} stock={7}/></Row>
-            </Card.Body>
-        </Card>
-    </Row>
+    <Container className='justify-content-center col-6'>
+        <div className='justify-content-center'>
+            <Card className="m-5">
+                <Row>
+                    <img src={props.image} alt="" width={size} height={size}/>
+                </Row>
+                <Card.Title as="h5">{props.title}</Card.Title>
+                <Card.Body>
+                    <Card.Text>
+                        {props.description}
+                    </Card.Text>
+                    <Card.Text>
+                        {props.price}
+                    </Card.Text>
+                    <Row><ItemCount initial={1} stock={7}/></Row>
+                </Card.Body>
+            </Card>
+        </div>
+    </Container>
   )
 }
